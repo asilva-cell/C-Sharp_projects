7385dd9145949ebd7fce9928ad4bc882132d51ae
Adriana Silva <adriana_silva@qbsol.com>
2020-04-01 19:07:25 -0600

    initial commit

---
+++ MyBank/MyBank/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    public class BanckAccount
+    {
+        //properties
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        //constructor
+        public BanckAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        //methods
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount to deposit must be a positive value.");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be a positive value.");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("No sufficient funds for this withdrawal.");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\t\tAmount\t\tNote");
+            foreach (var item in allTransactions)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
+
---
+++ MyBank/MyBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BanckAccount("Adriana", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Groceries");
+            Console.WriteLine($"Your new balance is {account.Balance}.");
+
+            account.MakeDeposit(3120, DateTime.Now, "Tax Return");
+            Console.WriteLine($"Your new balance is {account.Balance}.");
+
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            ////check that initial balance must be >0 - IF YIU CATCH IT, IT KEEPS RUNNING
+            //try
+            //{
+            //    var invalidAcc = new BanckAccount("invalid", -10000);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught: You're creating an account with negative balance.");
+            //    Console.WriteLine(e.ToString());
+
+            //}
+
+            ////This still runs: IF YIU CATCH IT, IT KEEPS RUNNING
+            //account.MakeWithdrawal(100, DateTime.Now, "Shoes");
+            //Console.WriteLine($"Your new balance is {account.Balance}.");
+
+
+        }
+    }
+}
